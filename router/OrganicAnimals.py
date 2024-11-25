6bbfb4e4dd8e47956cb333ca94166e9e757bc964
overloper <overloper0225@gmail.com>
2024-11-25 17:14:10 +0900

    update

--- router/OrganicAnimals.py
+++ router/OrganicAnimals.py
@@ -9,14 +9,14 @@ router = APIRouter(
 )
 
 load_dotenv()
-APIKEY = os.environ.get("API_KEY")
+APIKEY = os.environ.get("ANI_KEY")
 
 """
     전국동물보호센터정보표준데이터 API 요청 함수
 """
 def get_orgainc_animal_API():
   url = 'http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic'
-  params ={'serviceKey' : 'YRBQ33idQk0cZ1SPwsVTAa6b95yUEufXn2VAJ6pHSYAyGxSMucJwP6ctB7AQRoT9rpU2SDS2782LYGX/N42j1g==', 'care_reg_no' : '', 'care_nm' : '', 'numOfRows' : '1', 'pageNo' : '1', '_type' : 'json' }
+  params ={'serviceKey' : ''.format(APIKEY), 'care_reg_no' : '', 'care_nm' : '', 'numOfRows' : '1', 'pageNo' : '1', '_type' : 'json' }
   response = httpx.get(url=url, params=params)
   data =  json.loads(response.text)
   print(data["response"]["body"])
